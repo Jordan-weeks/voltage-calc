c963c5d1ccbe2b239a7dbe11b267c3677e187b86
Jordan Weeks <jdweeks2010@gmail.com>
2022-11-06 14:27:34 -0800

    phase color chart complete

--- src/pages/phase_colors/components/ColorTable.jsx
+++ src/pages/phase_colors/components/ColorTable.jsx
@@ -17,17 +17,148 @@ const ColorTable = ({aColor, bColor, cColor, cFont}) => {
                     <TableCell sx={{backgroundColor:`${bColor}`, color:'white'}}>3</TableCell>
                     <TableCell sx={{backgroundColor:`${bColor}`, color:'white'}}>45</TableCell>
                     <TableCell sx={{backgroundColor:`${bColor}`, color:'white'}}>{`${bColor}`}</TableCell>
-                    <TableCell sx={{backgroundColor:`${bColor}`, color:'white'}}>4</TableCell>
                     <TableCell sx={{backgroundColor:`${bColor}`, color:'white'}}>46</TableCell>
+                    <TableCell sx={{backgroundColor:`${bColor}`, color:'white'}}>4</TableCell>
                 </TableRow>
                 <TableRow>
                     <TableCell sx={{backgroundColor:`${cColor}`, color:`${cFont}`}}>5</TableCell>
                     <TableCell sx={{backgroundColor:`${cColor}`, color:`${cFont}`}}>47</TableCell>
                     <TableCell sx={{backgroundColor:`${cColor}`, color:`${cFont}`}}>{`${cColor}`}</TableCell>
-                    <TableCell sx={{backgroundColor:`${cColor}`, color:`${cFont}`}}>6</TableCell>
                     <TableCell sx={{backgroundColor:`${cColor}`, color:`${cFont}`}}>48</TableCell>
+                    <TableCell sx={{backgroundColor:`${cColor}`, color:`${cFont}`}}>6</TableCell>
+                </TableRow>
+
+                <TableRow>
+                    <TableCell sx={{backgroundColor:`${aColor}`, color:'white'}}>7</TableCell>
+                    <TableCell sx={{backgroundColor:`${aColor}`, color:'white'}}>49</TableCell>
+                    <TableCell sx={{backgroundColor:`${aColor}`, color:'white'}}>{`${aColor}`}</TableCell>
+                    <TableCell sx={{backgroundColor:`${aColor}`, color:'white'}}>50</TableCell>
+                    <TableCell sx={{backgroundColor:`${aColor}`, color:'white'}}>8</TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell sx={{backgroundColor:`${bColor}`, color:'white'}}>9</TableCell>
+                    <TableCell sx={{backgroundColor:`${bColor}`, color:'white'}}>51</TableCell>
+                    <TableCell sx={{backgroundColor:`${bColor}`, color:'white'}}>{`${bColor}`}</TableCell>
+                    <TableCell sx={{backgroundColor:`${bColor}`, color:'white'}}>52</TableCell>
+                    <TableCell sx={{backgroundColor:`${bColor}`, color:'white'}}>10</TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell sx={{backgroundColor:`${cColor}`, color:`${cFont}`}}>11</TableCell>
+                    <TableCell sx={{backgroundColor:`${cColor}`, color:`${cFont}`}}>53</TableCell>
+                    <TableCell sx={{backgroundColor:`${cColor}`, color:`${cFont}`}}>{`${cColor}`}</TableCell>
+                    <TableCell sx={{backgroundColor:`${cColor}`, color:`${cFont}`}}>54</TableCell>
+                    <TableCell sx={{backgroundColor:`${cColor}`, color:`${cFont}`}}>12</TableCell>
+                </TableRow>
+
+                <TableRow>
+                    <TableCell sx={{backgroundColor:`${aColor}`, color:'white'}}>13</TableCell>
+                    <TableCell sx={{backgroundColor:`${aColor}`, color:'white'}}>55</TableCell>
+                    <TableCell sx={{backgroundColor:`${aColor}`, color:'white'}}>{`${aColor}`}</TableCell>
+                    <TableCell sx={{backgroundColor:`${aColor}`, color:'white'}}>56</TableCell>
+                    <TableCell sx={{backgroundColor:`${aColor}`, color:'white'}}>14</TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell sx={{backgroundColor:`${bColor}`, color:'white'}}>15</TableCell>
+                    <TableCell sx={{backgroundColor:`${bColor}`, color:'white'}}>57</TableCell>
+                    <TableCell sx={{backgroundColor:`${bColor}`, color:'white'}}>{`${bColor}`}</TableCell>
+                    <TableCell sx={{backgroundColor:`${bColor}`, color:'white'}}>58</TableCell>
+                    <TableCell sx={{backgroundColor:`${bColor}`, color:'white'}}>16</TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell sx={{backgroundColor:`${cColor}`, color:`${cFont}`}}>17</TableCell>
+                    <TableCell sx={{backgroundColor:`${cColor}`, color:`${cFont}`}}>59</TableCell>
+                    <TableCell sx={{backgroundColor:`${cColor}`, color:`${cFont}`}}>{`${cColor}`}</TableCell>
+                    <TableCell sx={{backgroundColor:`${cColor}`, color:`${cFont}`}}>60</TableCell>
+                    <TableCell sx={{backgroundColor:`${cColor}`, color:`${cFont}`}}>18</TableCell>
+                </TableRow>
+
+                <TableRow>
+                    <TableCell sx={{backgroundColor:`${aColor}`, color:'white'}}>19</TableCell>
+                    <TableCell sx={{backgroundColor:`${aColor}`, color:'white'}}>61</TableCell>
+                    <TableCell sx={{backgroundColor:`${aColor}`, color:'white'}}>{`${aColor}`}</TableCell>
+                    <TableCell sx={{backgroundColor:`${aColor}`, color:'white'}}>62</TableCell>
+                    <TableCell sx={{backgroundColor:`${aColor}`, color:'white'}}>20</TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell sx={{backgroundColor:`${bColor}`, color:'white'}}>21</TableCell>
+                    <TableCell sx={{backgroundColor:`${bColor}`, color:'white'}}>63</TableCell>
+                    <TableCell sx={{backgroundColor:`${bColor}`, color:'white'}}>{`${bColor}`}</TableCell>
+                    <TableCell sx={{backgroundColor:`${bColor}`, color:'white'}}>64</TableCell>
+                    <TableCell sx={{backgroundColor:`${bColor}`, color:'white'}}>22</TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell sx={{backgroundColor:`${cColor}`, color:`${cFont}`}}>23</TableCell>
+                    <TableCell sx={{backgroundColor:`${cColor}`, color:`${cFont}`}}>65</TableCell>
+                    <TableCell sx={{backgroundColor:`${cColor}`, color:`${cFont}`}}>{`${cColor}`}</TableCell>
+                    <TableCell sx={{backgroundColor:`${cColor}`, color:`${cFont}`}}>66</TableCell>
+                    <TableCell sx={{backgroundColor:`${cColor}`, color:`${cFont}`}}>24</TableCell>
+                </TableRow>
+
+                <TableRow>
+                    <TableCell sx={{backgroundColor:`${aColor}`, color:'white'}}>25</TableCell>
+                    <TableCell sx={{backgroundColor:`${aColor}`, color:'white'}}>67</TableCell>
+                    <TableCell sx={{backgroundColor:`${aColor}`, color:'white'}}>{`${aColor}`}</TableCell>
+                    <TableCell sx={{backgroundColor:`${aColor}`, color:'white'}}>68</TableCell>
+                    <TableCell sx={{backgroundColor:`${aColor}`, color:'white'}}>26</TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell sx={{backgroundColor:`${bColor}`, color:'white'}}>27</TableCell>
+                    <TableCell sx={{backgroundColor:`${bColor}`, color:'white'}}>69</TableCell>
+                    <TableCell sx={{backgroundColor:`${bColor}`, color:'white'}}>{`${bColor}`}</TableCell>
+                    <TableCell sx={{backgroundColor:`${bColor}`, color:'white'}}>70</TableCell>
+                    <TableCell sx={{backgroundColor:`${bColor}`, color:'white'}}>28</TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell sx={{backgroundColor:`${cColor}`, color:`${cFont}`}}>29</TableCell>
+                    <TableCell sx={{backgroundColor:`${cColor}`, color:`${cFont}`}}>71</TableCell>
+                    <TableCell sx={{backgroundColor:`${cColor}`, color:`${cFont}`}}>{`${cColor}`}</TableCell>
+                    <TableCell sx={{backgroundColor:`${cColor}`, color:`${cFont}`}}>72</TableCell>
+                    <TableCell sx={{backgroundColor:`${cColor}`, color:`${cFont}`}}>30</TableCell>
                 </TableRow>
                 
+                <TableRow>
+                    <TableCell sx={{backgroundColor:`${aColor}`, color:'white'}}>31</TableCell>
+                    <TableCell sx={{backgroundColor:`${aColor}`, color:'white'}}>73</TableCell>
+                    <TableCell sx={{backgroundColor:`${aColor}`, color:'white'}}>{`${aColor}`}</TableCell>
+                    <TableCell sx={{backgroundColor:`${aColor}`, color:'white'}}>74</TableCell>
+                    <TableCell sx={{backgroundColor:`${aColor}`, color:'white'}}>32</TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell sx={{backgroundColor:`${bColor}`, color:'white'}}>33</TableCell>
+                    <TableCell sx={{backgroundColor:`${bColor}`, color:'white'}}>75</TableCell>
+                    <TableCell sx={{backgroundColor:`${bColor}`, color:'white'}}>{`${bColor}`}</TableCell>
+                    <TableCell sx={{backgroundColor:`${bColor}`, color:'white'}}>76</TableCell>
+                    <TableCell sx={{backgroundColor:`${bColor}`, color:'white'}}>34</TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell sx={{backgroundColor:`${cColor}`, color:`${cFont}`}}>35</TableCell>
+                    <TableCell sx={{backgroundColor:`${cColor}`, color:`${cFont}`}}>77</TableCell>
+                    <TableCell sx={{backgroundColor:`${cColor}`, color:`${cFont}`}}>{`${cColor}`}</TableCell>
+                    <TableCell sx={{backgroundColor:`${cColor}`, color:`${cFont}`}}>78</TableCell>
+                    <TableCell sx={{backgroundColor:`${cColor}`, color:`${cFont}`}}>36</TableCell>
+                </TableRow>
+
+                <TableRow>
+                    <TableCell sx={{backgroundColor:`${aColor}`, color:'white'}}>37</TableCell>
+                    <TableCell sx={{backgroundColor:`${aColor}`, color:'white'}}>79</TableCell>
+                    <TableCell sx={{backgroundColor:`${aColor}`, color:'white'}}>{`${aColor}`}</TableCell>
+                    <TableCell sx={{backgroundColor:`${aColor}`, color:'white'}}>80</TableCell>
+                    <TableCell sx={{backgroundColor:`${aColor}`, color:'white'}}>38</TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell sx={{backgroundColor:`${bColor}`, color:'white'}}>39</TableCell>
+                    <TableCell sx={{backgroundColor:`${bColor}`, color:'white'}}>81</TableCell>
+                    <TableCell sx={{backgroundColor:`${bColor}`, color:'white'}}>{`${bColor}`}</TableCell>
+                    <TableCell sx={{backgroundColor:`${bColor}`, color:'white'}}>82</TableCell>
+                    <TableCell sx={{backgroundColor:`${bColor}`, color:'white'}}>40</TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell sx={{backgroundColor:`${cColor}`, color:`${cFont}`}}>41</TableCell>
+                    <TableCell sx={{backgroundColor:`${cColor}`, color:`${cFont}`}}>83</TableCell>
+                    <TableCell sx={{backgroundColor:`${cColor}`, color:`${cFont}`}}>{`${cColor}`}</TableCell>
+                    <TableCell sx={{backgroundColor:`${cColor}`, color:`${cFont}`}}>84</TableCell>
+                    <TableCell sx={{backgroundColor:`${cColor}`, color:`${cFont}`}}>42</TableCell>
+                </TableRow>
             </TableBody>
         </Table>
   )
--- src/pages/phase_colors/index.js
+++ src/pages/phase_colors/index.js
@@ -1,18 +1,11 @@
 import React, { useState } from 'react'
 import {TableContainer, Stack, Typography, Switch, Table, TableHead, TableRow, TableCell, TableBody, TableFooter } from '@mui/material'
 import ColorTable from './components/ColorTable'
+import './phase-colors.css'
 
 
 const PhaseColors = () => {
-    const aPhase = [1,2,7,8,13,14]
-    const bPhase = [3,4,9,10,15,16]
-    const cPhase = [5,6,11,12,17,18]
-    const allPhases = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-    const generateNumbers = () =>{
-        for (let i = 1; i<100; i+3){
-            aPhase.push(i)
-        }
-    }
+    
     const [aColor, setAColor] = useState('Black')
     const [bColor, setBColor] = useState('Red')
     const [cColor, setCColor] = useState('Blue')
@@ -37,14 +30,12 @@ const PhaseColors = () => {
     }
   return (
     <>
-    <TableContainer>
-    <Stack direction="row" spacing={.5} alignItems="center" justifyContent={'center'} mt={2} mx={1}>
-        <Typography>Copper</Typography>
-        <Switch  color="default"  checked={conductorColors} onChange={changeConductorColors}></Switch>
-        <Typography>Aluminum</Typography>
-        
-        <Typography>View both</Typography>
-      </Stack>
+    <TableContainer className='phase-color-container'>
+        <Stack direction="row" spacing={.5} alignItems="center" justifyContent={'center'} mt={2} mx={1}>
+            <Typography>120/208v</Typography>
+            <Switch  color="default"  checked={conductorColors} onChange={changeConductorColors}></Switch>
+            <Typography>277/480V</Typography>
+        </Stack>
         <ColorTable 
             aColor = {aColor}
             bColor = {bColor}
